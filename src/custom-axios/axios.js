6562eb58470536bce0768e2b929178bf02d2bcff
vristovski <vaci.ristovski@gmail.com>
2023-04-19 20:35:21 +0200

    changes

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9094/api',
+    baseURL: 'https://emtlabbackend.herokuapp.com/',
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
